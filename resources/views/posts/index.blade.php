90e371b7d5bf0704964bb450b577b1addcc42301
richusx <me@richusx.me>
2020-08-25 22:51:10 +0300

    Added captions to posts in feed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,6 +25,12 @@
                         <a href="{{ @route('post.show', ['post'=>$post->id]) }}"><img class="card-img" style="border-radius: 0 !important;" src="/storage/{{ $post->image }}"></a>
                         <div class="card-body">
                             <like-button post-id="{{ $post->id }}" liked="{{ $likes->contains($post->id) }}" likes="{{ $post->liked_by->count() }}"></like-button>
+                            <p>
+                                <a href="{{ @route('profile.show', ['username' => $post->user->username]) }}" >
+                                    <span class="text-dark font-weight-bold">{{ $post->user->username }}</span>
+                                </a>
+                                {{ $post->caption }}
+                            </p>
                             <div class="text-muted small pt-1">{{ $post->created_at_string() }}</div>
                         </div>
 
@@ -32,41 +38,6 @@
                 </div>
             </div>
 
-
-{{--            <div class="row">--}}
-{{--                <div class="col-8 offset-2">--}}
-{{--                    <img src="/storage/{{ $post->image }}" class="img-fluid" alt="">--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-4">--}}
-
-{{--                        <div class="d-flex align-items-center">--}}
-{{--                            <div class="mr-3">--}}
-{{--                                <a href="{{ @route('profile.show', ['username' => $post->user->username]) }}" >--}}
-{{--                                    <img src="{{ $post->user->profile->profileImage() }}" alt="{{ $post->user->username}}'s profile picture" class="rounded-circle w-100" style="max-width: 40px;">--}}
-{{--                                </a>--}}
-{{--                            </div>--}}
-{{--                            <div class="font-weight-bold">--}}
-{{--                                <a href="{{ @route('profile.show', ['username' => $post->user->username]) }}" >--}}
-{{--                                    <span class="text-dark">{{ $post->user->username }}</span>--}}
-{{--                                </a>--}}
-{{--                                @if($post->user_id != Auth::user()->id)--}}
-{{--                                    <span>•</span>--}}
-{{--                                    <a href="#">Follow</a>--}}
-{{--                                @endif--}}
-
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    <hr>--}}
-{{--                    <p>--}}
-{{--                        <a href="{{ @route('profile.show', ['username' => $post->user->username]) }}" >--}}
-{{--                            <span class="font-weight-bold text-dark">{{ $post->user->username }}</span>--}}
-{{--                        </a>--}}
-{{--                        {{ $post->caption }}--}}
-{{--                    </p>--}}
-{{--                </div>--}}
-{{--            </div>--}}
         @endforeach
     </div>
 @endsection
