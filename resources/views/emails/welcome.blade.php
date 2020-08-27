ba336bf4e7f5aa2b67c39d3050d9ec7c291a95c4
richusx <me@richusx.me>
2020-08-27 19:37:21 +0300

    Added a welcome email

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to CloneGram!
+
+## Hey, {{ $user->username }}!
+Thank you for joining us at CloneGram. Watch this space for new exciting things coming soon!
+
+All the best,<br>
+Ritvars
+@endcomponent
